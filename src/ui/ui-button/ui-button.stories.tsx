5c75c83273f2533d465ed27d1450c3cbe6e807cf
Alonso Velez <alonso.velez@rappi.com>
2022-08-27 15:07:06 -0500

    UI Button variations

--- src/ui/ui-button/ui-button.stories.tsx
+++ src/ui/ui-button/ui-button.stories.tsx
@@ -8,14 +8,58 @@ export default {
 } as ComponentMeta<typeof UiButton>;
 
 const Template: ComponentStory<typeof UiButton> = (args) => <UiButton {...args} />;
+const children = 'Hola mundo';
 
 export const Default = Template.bind({});
 Default.args = {
-  text: 'Text',
-  isExpert: false,
-  isDisabled: false,
-  isSecondary: false,
-  isLoading: false,
-  iconRight: <span className="icon-share" />,
-  onClick: () => {},
+  children: children,
+  icon: <span className="icon-share" />
+}
+
+export const Rounded = Template.bind({});
+Rounded.args = {
+  appearance:'rounded',
+  children: children,
+  icon: <span className="icon-share" />
+}
+
+export const Outline = Template.bind({});
+Outline.args = {
+  appearance:'outline',
+  children: children,
+  icon: <span className="icon-share" />,
+}
+
+export const Expert = Template.bind({});
+Expert.args = {
+  appearance: 'expert',
+  children: children,
+  icon: <span className="icon-share" />
+}
+
+export const Custom = Template.bind({});
+Custom.args = {
+  children: children,
+  icon: <span className="icon-share" />,
+  styles: {
+    width: '500px',
+    height: '50px',
+    borderRadius: '10px',
+    background: '#A59BFD',
+    color: '#000000'
+  }
+}
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: children,
+  disabled: true,
+  icon: <span className="icon-share" />
+}
+
+export const Loading = Template.bind({});
+Loading.args = {
+  children: children,
+  isLoading: true,
+  icon: <span className="icon-share" />
 }
