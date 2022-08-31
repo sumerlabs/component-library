4fa7a12d9fb75a51e4b5a34b2cb266ccd24b2bfc
Yamid <yamid@sumerlabs.com>
2022-08-31 16:58:51 -0300

    Fix del type en onclick

--- src/ui/ui-button/types.ts
+++ src/ui/ui-button/types.ts
@@ -1,14 +1,14 @@
-import { MouseEvent, ReactNode } from "react";
+import { MouseEvent, ReactNode, FormEvent } from "react";
 
 export type UiButtonProps = {
 	appearance: string;
 	children: string | React.ReactNode;
 	className?: string;
 	disabled?: boolean;
-  isLoading?: boolean;
-  icon?: ReactNode;
-	onClick: (event?: MouseEvent<HTMLButtonElement>) => void;
-  styles?: ButtonStyles;
+    isLoading?: boolean;
+    icon?: ReactNode;
+	onClick: (event?: MouseEvent<HTMLButtonElement> | FormEvent<HTMLFormElement>) => void;
+    styles?: ButtonStyles;
 };
 
 export type StyledButtonProps = ButtonStyles & {
