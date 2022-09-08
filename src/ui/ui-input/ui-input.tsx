c7d8c6c81fa6c573bcc81ec5efcf0eb9a2acda73
Alonso Velez <alonso.velez@rappi.com>
2022-09-08 18:05:29 -0500

    Ui Input documentation

--- src/ui/ui-input/ui-input.tsx
+++ src/ui/ui-input/ui-input.tsx
@@ -1,24 +1,40 @@
-import React, { ChangeEvent, FC } from "react";
+import React, { ChangeEvent, FC, useEffect, useRef, useState } from "react";
 import { Wrapper } from "./ui-input.styled";
 import { UiInputProps } from "./types";
  
 const UiInput: FC<UiInputProps> = ({
   className,
   hint,
-  label,
   type = 'text',
+  style = 'outline',
   name,
   value,
   placeholder,
+  label,
+  prefix,
+  disabled,
   hasError,
+  success,
   maxLength,
+  minLength,
   errorMessage,
   onChange,
   onFocus,
   onBlur
 }) => {
 
+  const inputRef = useRef<HTMLInputElement| null>(null);
+  const [isActive, setIsActive] = useState(false);
+  const [showPasword, setShowPassword] = useState(false);
+
+  useEffect(() => {
+    if(inputRef.current && inputRef.current.value.length > 0){
+      inputRef.current.classList.toggle('fill')
+    }
+  }, [])
+  
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setIsActive(e.target.value.length > 0);
     if (e.target.maxLength != -1) {
       if (e.target.maxLength >= e.target.value.length) onChange(e);
     } else {
@@ -27,20 +43,28 @@ const UiInput: FC<UiInputProps> = ({
   }
 
   return (
-    <Wrapper className={`${className} ${!!hasError && 'error'}`}>
-      <div className="input-box">
+    <Wrapper className={`${className} ${!!hasError && 'error'} ${!!success && 'success'}`}>
+      <div className={`input-box ${style} ${prefix && 'is-prefix'}`}>
+        {prefix && <span className="prefix">{prefix}</span>}
         <input 
-          type={type}
-          className="input" 
+          ref={inputRef}
+          type={`${showPasword ? 'text' : type}`}
+          className={`input ${isActive && 'fill'} ${!!placeholder && 'is-placeholder'}`} 
           name={name}
           value={value}
+          disabled={disabled}
           onBlur={onBlur}
           onFocus={onFocus}
           onChange={handleChange}
           maxLength={maxLength}
-          placeholder={placeholder ? placeholder : label}
+          minLength={minLength}
+          placeholder={placeholder}
         />
         {!!label && <span className="label">{label}</span>}
+        {type === 'password' && (
+          <span onClick={() => setShowPassword(s => !s)} 
+                className={`show-password ${showPasword ? 'icon-hide-password' : 'icon-show-password'}`}/>
+        )}
       </div>
       {(!!hint || hasError) && <div className="hint">{errorMessage || hint}</div>}
     </Wrapper>
